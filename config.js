955662ec99d502724989a898188a01412668f5d1
Abhi <AbhinavA10@users.noreply.github.com>
2017-12-18 01:08:12 -0500

    added back config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         '8q75gKqMWUsB8deaiedXQn4KX',
+  consumer_secret:      'qHjd12zsCnNbvetqlt8CGGHKj8N2G9fNuncUr3Cmd2GiZSIGRC',
+  access_token:         '940749626629545984-MRWZKNLLmZqYBfESd3Zr6Z8kRfSrnK1',
+  access_token_secret:  'rrvlojH8cGiRi3K0DGienBqfkUzLoSxTCt5xyqmGNsXZK',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+
+}
